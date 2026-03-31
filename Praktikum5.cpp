aadff8377dc5d3d9d3005dc153321a0743ac1cb2
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-03-31 23:39:15 +0800

    Menambahkan fungsi untuk mengecek bilangan prima

--- Praktikum5.cpp
+++ Praktikum5.cpp
@@ -5,3 +5,8 @@ using namespace std;
 //Variabel global sesuai spesifikasi
 int globalAngka = 0;
 int globalPilihan = 0;
+
+//Function untuk mengecek bilangan prima menggunakan while loop
+bool cekPrima(int n){
+    
+}
